f455345ba03cb76c6db9aaa947d4f38a249d34cb
viperk17 <pk.airsqn@protonmail.com>
2021-05-13 16:34:18 +0530

    problems

---
+++ python.py
@@ -0,0 +1,83 @@
+# lis = [1,2,3,112,45,64,21]
+# a=sorted(lis, reverse=True)
+# print(a)
+#
+# b = lis.reverse()
+# print(b)
+##################################################################
+# Fibonacci Series
+# def fib(n):
+#     z, b = 0, 1
+#
+#     result = []
+#     while z < n:
+#         print(z, end=" "+ '\n')
+#         result.append(z)
+#         z,b = b, z+b
+#     return result
+#
+# fib20 = fib(20)
+# print(fib20)
+
+############################################
+# x = input(int("Please enter an integer: "))
+# if x<0:
+#     x=0
+#     print(x)
+# elif x==0:
+#     print('zero')
+# elif x==1:
+#     print('Single')
+# else:
+#     print('More')
+############################################
+# words = ['cat','dpg','w','lomix']
+# for w in words:
+#     print(w, len(w))
+############################################
+# words = ['cat','dpg','w','lomix']
+# for o in range(len(words)):
+#     print(o, words[o])
+
+############################################
+# def f(a, L=[]):
+#     L.append(a)
+#     return L
+#
+# print(f(1))
+# print(f(121))
+# print(f(11))
+############################################
+# def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+#     print("-- This parrot wouldn't", action, end=' ')
+#     print("if you put", voltage, "volts through it.")
+#     print("-- Lovely plumage, the", type)
+#     print("-- It's", state, "!")
+#
+# parrot(1000)
+############################################
+i=5
+
+def f(arg=i):
+    print(arg)
+
+i=7
+f(4)
+
+############################################
+
+############################################
+
+
+############################################
+
+############################################
+
+
+############################################
+
+############################################
+
+############################################
+
+############################################
